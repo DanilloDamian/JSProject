9d43b7cca68da00561b436df8049984e7e4db581
Danillo <danillo.damian@hotmail.com>
2022-09-11 10:12:17 -0300

    Added delete confirmation

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -1,9 +1,17 @@
 const tabela = document.querySelector("table");
 
 tabela.addEventListener("dblclick",function(event){
+    let x;
+    let r= confirm("Tem certeza que deseja excluir paciente?");
+    if(r==true){
     event.target.parentNode.classList.add("fadeOut");
     setTimeout(function(){
         event.target.parentNode.remove();
     }, 500);
+    }else{
+        alert("Você desistiu de excluir o paciente");
+    }
 });
+
+
 
